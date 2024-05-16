a30fb3f2d438e9492dce65eac7bb908894cae7d0
Yakov N <yakov@liberateinc.com>
2024-05-16 16:41:08 -0400

    remove unnecessary log

--- vocode/streaming/streaming_conversation.py
+++ vocode/streaming/streaming_conversation.py
@@ -155,10 +155,11 @@ class StreamingConversation(Generic[OutputDeviceType]):
             elif transcription.message.strip() == "":
                 self.conversation.logger.info("Ignoring empty transcription")
                 self.conversation.is_human_speaking = False
-                return
+                # return
             # otherwise mark the message as last time the human spoke
             else:
                 self.conversation.mark_last_final_transcript_from_human()
+                self.conversation.is_human_speaking = True
             # If the human is not speaking but there is an interrupt,
             # we should send the interrupt to the agent and log that the human is speaking
             # case: this happens when there is an agent speaking and the human needs to interrupt them.
@@ -203,7 +204,6 @@ class StreamingConversation(Generic[OutputDeviceType]):
             # else:
             #     self.kill_tasks_when_human_is_talking()
             #     self.conversation.broadcast_interrupt()
-            self.conversation.logger.debug(f"*******Is a human speaking currenltly: {self.conversation.is_human_speaking}*****")
 
     class FillerAudioWorker(InterruptibleAgentResponseWorker):
         """
